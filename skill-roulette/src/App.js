64fd10cd2986c26e019dcabb1d97c35372c2a96b
CJ S <cjsriram05@gmail.com>
2025-11-25 00:32:12 -0500

    Supabase Helpers + Cheat Sheet + Testing Apparatus

--- skill-roulette/src/App.js
+++ skill-roulette/src/App.js
@@ -3,3 +3,12 @@ import LoginPage from "./components/LoginPage";
 export default function App() {
   return <LoginPage />;
 }
+
+// Uncomment the following lines to use the DbHelpersTestPage instead of LoginPage
+/*
+import DbHelpersTestPage from "./dev/DbHelpersTestPage";
+
+export default function App() {
+  return <DbHelpersTestPage />;
+}
+*/
