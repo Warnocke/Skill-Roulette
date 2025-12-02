482b14198059cedb94f7e25e62a036ca935e3d31
frozehu <166183471+frozehu@users.noreply.github.com>
2025-12-02 16:36:48 -0500

    added more stuff i forgot before

--- skill-roulette/src/App.js
+++ skill-roulette/src/App.js
@@ -1,14 +1,30 @@
+// import React from "react";
+// import LoginPage from "./components/LoginPage";
+// export default function App() {
+//   return <LoginPage />;
+// }
+
+
+// // Uncomment the following lines to use the DbHelpersTestPage instead of LoginPage
+// /*
+// import DbHelpersTestPage from "./dev/DbHelpersTestPage";
+
+// export default function App() {
+// return <DbHelpersTestPage />;
+// }
+// */
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
-export default function App() {
-  return <LoginPage />;
-}
-
-// Uncomment the following lines to use the DbHelpersTestPage instead of LoginPage
-/*
-import DbHelpersTestPage from "./dev/DbHelpersTestPage";
+import FeedPage from "./components/FeedPage";
 
 export default function App() {
-  return <DbHelpersTestPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/feed" element={<FeedPage />} />
+      </Routes>
+    </Router>
+  );
 }
-*/
