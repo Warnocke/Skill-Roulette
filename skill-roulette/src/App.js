48912d82993b9403b2827eefd733529cadce591b
Warnocke <evan.warnock@gmail.com>
2025-12-02 19:13:12 -0500

    protected feed

--- skill-roulette/src/App.js
+++ skill-roulette/src/App.js
@@ -1,29 +1,25 @@
-// import React from "react";
-// import LoginPage from "./components/LoginPage";
-// export default function App() {
-//   return <LoginPage />;
-// }
-
-
-// // Uncomment the following lines to use the DbHelpersTestPage instead of LoginPage
-// /*
-// import DbHelpersTestPage from "./dev/DbHelpersTestPage";
-
-// export default function App() {
-// return <DbHelpersTestPage />;
-// }
-// */
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import FeedPage from "./components/FeedPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Public route */}
         <Route path="/" element={<LoginPage />} />
-        <Route path="/feed" element={<FeedPage />} />
+
+        {/* Protected route */}
+        <Route
+          path="/feed"
+          element={
+            <ProtectedRoute>
+              <FeedPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
